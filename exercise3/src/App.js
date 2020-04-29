88dfc80c604f60d1a1b820d39de512a7e5ff8014
neternefer <alkarinqe@yahoo.com>
2020-04-29 08:57:42 +0200

    Add App.js code.

--- exercise3/src/App.js
+++ exercise3/src/App.js
@@ -1,6 +1,33 @@
-import React from 'react'
+import React from 'react';
+import Game from './Game';
+import Score from './Score';
 
 class App extends React.Component {
+  state = {
+    numCorrect = 0,
+    numQuestions = 0
+  };
 
-}
+  inputHandle = (userAnswer) => {
+    if(userAnswer) {
+      this.setState((currState) => ({
+        numCorrect : currState.numCorrect + 1
+      }));
+    }
+    this.setState((currState) => ({
+      numQuestions: currState.numQuestions + 1
+    }));
+  };
 
+  render() {
+    return (
+      <div className="dashboard">
+        <h2>Mental Math</h2>
+        <Game inputHandle={this.inputHandle} />
+        <Score numCorrect={this.numCorrect} numQuestions={this.numQuestions} />
+      </div>
+    )
+  }
+};
+
+export default App;
