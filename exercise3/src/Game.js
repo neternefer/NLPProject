a595f39d5e3670f277f13615e5f1d33fa8507145
neternefer <alkarinqe@yahoo.com>
2020-04-29 09:15:51 +0200

    Add resetGame function.

--- exercise3/src/Game.js
+++ exercise3/src/Game.js
@@ -17,6 +17,17 @@ class Game extends React.Component {
         const answer = Math.floor(Math.random() *3) + values.reduce((total, curr) => total + curr);
         return [...values, answer];
     };
+
+    resetGame = (newValues) => {
+        this.setState((currState) => ({
+            value1: newValues[0],
+            value2: newValues[1],
+            value3: newValues[2],
+            proposedAnswer: newValues[3]
+        }));
+    };
+
+
 }
 
 export default Game;
