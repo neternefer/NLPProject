4748e30e2266580d4bfb6d62753ddae76fd2cb49
neternefer <alkarinqe@yahoo.com>
2020-04-29 14:57:09 +0200

    Add Button and Form components code.

--- exercise5/src/App.js
+++ exercise5/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import Form from './Form'
 
 class App extends React.Component {
   state = {
---
+++ exercise5/src/Form.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react'
+import Button from './Button'
+
+class Form extends Component {
+    state = {
+        value: '',
+        items: [],
+    };
+
+    handleChange = event => {
+        this.setState({ value: event.target.value });
+    };
+
+    addItem = event => {
+        event.preventDefault();
+        this.setState(oldState => ({
+          items: [...oldState.items, this.state.value],
+        }));
+    };
+
+    deleteLastItem = event => {
+        this.setState(prevState => ({ items: this.state.items.slice(0, -1) }));
+      };
+
+    inputIsEmpty = () => {
+        return this.state.value === '';
+    };
+
+    noItemsFound = () => {
+        return this.state.items.length === 0;
+    };
+
+    render() {
+        return (
+            <div className="shopping-list">
+                <h2>Shopping List</h2>
+                <form onSubmit={this.addItem}>
+                <input
+                    type="text"
+                    placeholder="Enter New Item"
+                    value={this.state.value}
+                    onChange={this.handleChange}
+                />
+                <Button disabled={this.inputIsEmpty()}
+                value="Add"
+                onClick={this.addItem}>Add</Button>
+                </form>
+            </div>
+
+        )
+    }
+}
+
+export default Form
